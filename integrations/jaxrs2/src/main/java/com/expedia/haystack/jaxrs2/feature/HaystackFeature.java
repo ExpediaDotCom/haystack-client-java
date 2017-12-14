598b786d51e96ff726216db38d56e94c67055e63
Bryan Shell <bshell@expedia.com>
2017-12-14 15:53:16 -0600

    Add filters based on the runtime configuration of JAX-RS2

--- integrations/jaxrs2/src/main/java/com/expedia/haystack/jaxrs2/feature/HaystackFeature.java
+++ integrations/jaxrs2/src/main/java/com/expedia/haystack/jaxrs2/feature/HaystackFeature.java
@@ -1,8 +1,12 @@
 package com.expedia.haystack.jaxrs2.feature;
 
+import javax.ws.rs.RuntimeType;
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
 import javax.ws.rs.ext.Provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.expedia.haystack.jaxrs2.filters.ClientFilter;
 import com.expedia.haystack.jaxrs2.filters.ServerFilter;
@@ -11,6 +15,8 @@ import io.opentracing.Tracer;
 
 @Provider
 public class HaystackFeature implements Feature {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HaystackFeature.class);
+
     private final Tracer tracer;
 
     public HaystackFeature(Tracer tracer) {
@@ -23,8 +29,17 @@ public class HaystackFeature implements Feature {
             return false;
         }
 
-        context.register(new ServerFilter(tracer));
-        context.register(new ClientFilter(tracer));
+        switch (context.getConfiguration().getRuntimeType()) {
+        case SERVER:
+            context.register(new ServerFilter(tracer));
+            break;
+        case CLIENT:
+            context.register(new ClientFilter(tracer));
+            break;
+        default:
+            LOGGER.error("Unknown runtime ({}), not registering Haystack feature", context.getConfiguration().getRuntimeType());
+            return false;
+        }
         return true;
     }
 }
