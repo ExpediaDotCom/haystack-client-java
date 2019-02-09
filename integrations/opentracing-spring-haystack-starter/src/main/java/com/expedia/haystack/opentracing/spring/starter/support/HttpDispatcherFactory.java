4e5c95bfbc5dc7c1238a45dcbfb6053cabc76433
Magesh Chandramouli <mchandramouli@expedia.com>
2019-02-09 12:52:35 -0800

    removing spring integration and moving to top level

--- integrations/opentracing-spring-haystack-starter/src/main/java/com/expedia/haystack/opentracing/spring/starter/support/HttpDispatcherFactory.java
+++
@@ -1,9 +0,0 @@
-package com.expedia.haystack.opentracing.spring.starter.support;
-
-import com.expedia.haystack.opentracing.spring.starter.TracerSettings;
-import com.expedia.www.haystack.client.dispatchers.Dispatcher;
-import com.expedia.www.haystack.client.metrics.MetricsRegistry;
-
-public interface HttpDispatcherFactory {
-    Dispatcher create(MetricsRegistry metricsRegistry, TracerSettings.HttpConfiguration httpConfiguration);
-}
