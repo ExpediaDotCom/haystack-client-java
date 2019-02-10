ebc8ac0072f7aa688ea6f3d23838d6e289caa25b
Magesh Chandramouli <code.janitor.007@gmail.com>
2019-02-09 21:25:36 -0800

    removing spring integration and moving to top level (#92)
    * removing spring integration and moving to top level
    
    * removing examples
    
    * removing opencensus to top level

--- integrations/opentracing-spring-haystack-starter/src/main/java/com/expedia/haystack/opentracing/spring/starter/support/TracerCustomizer.java
+++
@@ -1,7 +0,0 @@
-package com.expedia.haystack.opentracing.spring.starter.support;
-
-import com.expedia.www.haystack.client.Tracer;
-
-public interface TracerCustomizer {
-    void customize(Tracer.Builder builder);
-}
