a9ecc0dad1bce324a4908995b15eee41f66599ef
Kapil Rastogi <krastogi@expedia.com>
2020-08-07 13:55:58 +0530

    Adding B3 compatible hexchar id generator (#114)
    * Adding B3 compatible hexchar id generator
    
    * Handling 0 while generating random longs
    
    * Handling 0 while generating random longs
    
    * Using ThreadLocalRandom to make it thread safe
    
    * Using ThreadLocalRandom to make it thread safe
    
    * Legacy method generate to not return null

---
+++ core/src/main/java/com/expedia/www/haystack/client/idgenerators/HexcharIdGenerator.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019 Expedia, Inc.
+ *
+ *       Licensed under the Apache License, Version 2.0 (the "License");
+ *       you may not use this file except in compliance with the License.
+ *       You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *       Unless required by applicable law or agreed to in writing, software
+ *       distributed under the License is distributed on an "AS IS" BASIS,
+ *       WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *       See the License for the specific language governing permissions and
+ *       limitations under the License.
+ *
+ */
+package com.expedia.www.haystack.client.idgenerators;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Generates random and unique B3 compatible hexchar ids.
+ * Note that traceId will be in 128 bit while spanId and parentSpanId will be 64 bit.
+ */
+public class HexcharIdGenerator implements IdGenerator {
+
+    @Override
+    public String generateTraceId() {
+        return String.format("%016X", nextRandomLong()).concat(String.format("%016X", nextRandomLong()));
+    }
+
+    @Override
+    public String generateSpanId() {
+        return String.format("%016X", nextRandomLong());
+    }
+
+    @Override
+    public Object generate() {
+        return String.format("%016X", nextRandomLong()).concat(String.format("%016X", nextRandomLong()));
+    }
+
+    /**
+     * Generates a new 64-bit id, taking care to dodge zero which can be confused with absent
+     */
+    private long nextRandomLong() {
+        long nextId = ThreadLocalRandom.current().nextLong(Long.MAX_VALUE);
+        while (nextId == 0L) {
+            nextId = ThreadLocalRandom.current().nextLong(Long.MAX_VALUE);
+        }
+        return nextId;
+    }
+}
